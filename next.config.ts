23e8329ad488ef6c3a326d241ffb688360eb18fa
Tim Dost <timdost@gmail.com>
2025-05-30 16:54:37 +0200

    Add domain configuration for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["timdost.com"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
